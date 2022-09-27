b1bcef2c4cfb22acf81e256732d762b5f3d7c6c2
phatnhse <phatnhse@gmail.com>
2022-09-27 20:05:37 +0700

    feature: lambda function with http api

---
+++ src/functions/index.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyResultV2, Context, APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayProxyEventV2,context: Context): Promise<APIGatewayProxyResultV2> => {
+	const result = {
+		message: 'Hello User!',
+		event,
+		context,
+	};
+	
+	return {
+		statusCode: 200,
+		body: JSON.stringify(result)
+	};
+};
---
+++ test/cdk_lambda_apigateway.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaApigateway from '../lib/cdk_lambda_apigateway-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_lambda_apigateway-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaApigateway.CdkLambdaApigatewayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
